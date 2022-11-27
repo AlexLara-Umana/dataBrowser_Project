8a935bf487d234765596d166536f272defe4fd4d
AlexLara-Umana <74322508+AlexLara-Umana@users.noreply.github.com>
2022-11-27 15:42:54 -0800

    Implementation

--- Insert.php
+++ Insert.php
@@ -1,18 +1,13 @@
 <?php
 
-include('./PokemonForm.php');
+include('./ChampionForm.php');
 
 function handleUserInsert() {
-    $json_str = file_get_contents("./data/pokemondb.json");
+    $json_str = file_get_contents("./Data/ChampionB.json");
     $objlist = json_decode($json_str);
 
-    $request = new PokemonForm();
+    $request = new ChampionForm();
     $request->populateFromPostData();
-
-
-
-
-
 }
 
 handleUserInsert();
